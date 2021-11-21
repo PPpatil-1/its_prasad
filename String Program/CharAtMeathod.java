5b1d309c3545ef3da1b8ac11f8e8c4d3ad862072
PPpatil-1 <94783040+PPpatil-1@users.noreply.github.com>
2021-11-21 15:48:38 +0530

    Create CharAtMeathod.java

---
+++ String Program/CharAtMeathod.java
@@ -0,0 +1,15 @@
+package stringStringBufferStringBuilder;
+
+public class CharAtMeathod {
+
+	public static void main(String[] args) {
+
+		String str =new String( "viraj");
+
+		char c = str.charAt(2);
+
+		System.out.println(str.charAt(1));// way to fetch out character with index
+
+		System.out.println(c);
+	}
+}
