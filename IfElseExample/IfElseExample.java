fc9742fdf7bea64a9c6c8f64abeed0264fe07f54
PPpatil-1 <94783040+PPpatil-1@users.noreply.github.com>
2021-11-24 19:07:24 +0530

    Create IfElseExample.java

---
+++ IfElseExample/IfElseExample.java
@@ -0,0 +1,12 @@
+package conditionalStatement;
+
+public class IfElseExample {
+	public static void main(String[] args) {
+		char a = 'm';
+		if (a == 'f') {
+			System.out.println("a is boy");
+		} else {
+			System.out.println("a is girl");
+		}
+	}
+}
